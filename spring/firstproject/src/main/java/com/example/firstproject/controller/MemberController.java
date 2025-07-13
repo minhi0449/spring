ce448e1b7564c282b2f8c052b2d4b7f2e1639456
minhi0449 <minhi0449@gmail.com>
2025-07-13 21:20:55 +0900

    refactor Member

--- spring/firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ spring/firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -21,15 +23,18 @@ public class MemberController {
 
     @PostMapping("/join")
     public String join(MemberForm memberForm){
-        System.out.println(memberForm.toString());
+        log.info(memberForm.toString());
+        // System.out.println(memberForm.toString());
 
         // 1. DTO를 엔티티로 변환
         Member member = memberForm.toEntity();
-        System.out.println(member.toString());
+        log.info(member.toString());
+        // System.out.println(member.toString());
 
         // 2. Repository 로 Entity 를 DB에 저장
         Member saved = memberRepository.save(member);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        // System.out.println(saved.toString());
         return "";
     }
 }
