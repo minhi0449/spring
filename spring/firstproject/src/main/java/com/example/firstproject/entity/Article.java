8f84817e077be14ba01c1ef93bf28bee16638415
minhi0449 <minhi0449@gmail.com>
2025-07-13 21:04:11 +0900

    refactoring Entity

--- spring/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ spring/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor // Article() 생성자를 대체하는 어노테이션 추가
+@ToString // toString() 메서드를 대체하는 어노테이션 추가
 public class Article {
     @Id // 엔티티의 대푯값 지정
     @GeneratedValue // 자동 생성 기능 추가(숫자가 자동으로 매겨짐)
@@ -16,21 +20,4 @@ public class Article {
 
     @Column // content 필드 선언, DB 테이블의 content 열과 연결됨
     private String content;
-
-    // Article 생성자 추가
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    // toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
