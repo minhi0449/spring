5b509049a072cb26d353e340dec6f3cddccb0160
minhi0449 <minhi0449@gmail.com>
2025-07-13 21:02:06 +0900

    refactoring DTO

--- spring/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ spring/firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,19 +1,15 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     private String title;   // 제목을 받을 필드
     private String content; // 내용을 받을 필드
 
-    // 데이터를 잘 받았는지 확인할 toString() 메서드 추가
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
