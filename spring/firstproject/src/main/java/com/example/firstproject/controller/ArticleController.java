5b4ed133030bf8bfabdf900718c6c1ece1556171
minhi0449 <minhi0449@gmail.com>
2025-07-13 15:27:12 +0900

    DTO → Entity

--- spring/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ spring/firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,9 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){ // 폼 데이터를 DTO로 받기
         System.out.println(form.toString()); // DTO에 폼 데이터가 잘 담겼는지 확인
+        // 1. DTO를 엔티티로 변환
+        Article article = form.toEntity();
+        // 2. 레피자토리로 엔티티를 DB에 저장
         return "";
     }
 
