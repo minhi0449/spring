f4ac543fedb0d48f6b61b078d3b33ce69ec7f9ab
minhi0449 <minhi0449@gmail.com>
2025-07-14 16:18:56 +0900

    ch01

---
+++ spring/ch01/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * 날짜 : 2025/07/14
+ * 이름 : 김민희
+ * 내용 : Spring 개요 및 개발환경 구축 실습
+ *
+ * 개발환경
+ * - intellij Ultimate
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Spring!");
+    }
+}
