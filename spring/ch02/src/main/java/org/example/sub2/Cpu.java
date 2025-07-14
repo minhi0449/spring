be82ab421970cb76835cc2b718a82de6cc112f48
minhi0449 <minhi0449@gmail.com>
2025-07-15 01:02:09 +0900

    AppConfig 설정

--- spring/ch02/src/main/java/org/example/sub2/Cpu.java
+++ spring/ch02/src/main/java/org/example/sub2/Cpu.java
@@ -1,5 +1,8 @@
 package org.example.sub2;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Cpu {
     public void show(){
         System.out.println("CPU : Intel i9");
--- spring/ch02/src/main/java/org/example/sub2/Hdd.java
+++ spring/ch02/src/main/java/org/example/sub2/Hdd.java
@@ -1,5 +1,8 @@
 package org.example.sub2;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Hdd {
     public void show(){
         System.out.println("HDD : Samsung 4TB");
