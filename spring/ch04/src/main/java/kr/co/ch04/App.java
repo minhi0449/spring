8ea435fa4f2c01a9041d10abfb53040a0a83641f
minhi0449 <minhi0449@gmail.com>
2025-07-19 23:38:54 +0900

    pom.xml

---
+++ spring/ch04/src/main/java/kr/co/ch04/App.java
@@ -0,0 +1,13 @@
+package kr.co.ch04;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ spring/ch04/src/main/java/kr/co/ch04/config/AppConfig.java
@@ -0,0 +1,5 @@
+package kr.co.ch04.config;
+
+
+public class AppConfig {
+}
