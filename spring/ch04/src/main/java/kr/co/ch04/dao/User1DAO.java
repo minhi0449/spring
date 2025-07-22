58150a462b037acd32822c7b41ff02c56dab7dd3
minhi0449 <minhi0449@gmail.com>
2025-07-22 09:19:48 +0900

    ch04

--- spring/ch04/src/main/java/kr/co/ch04/dao/User1DAO.java
+++ spring/ch04/src/main/java/kr/co/ch04/dao/User1DAO.java
@@ -1,4 +1,25 @@
 package kr.co.ch04.dao;
 
+import kr.co.ch04.dto.User1DTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class User1DAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public void insertUser1(User1DTO dto){
+        String sql = "insert into user1 values(?,?,?,?)";
+        Object[] params = {
+                dto.getUid(),
+                dto.getName(),
+                dto.getHp(),
+                dto.getAge()
+        };
+        jdbcTemplate.update(sql, params);
+    }
+
+
 }
--- spring/ch04/src/main/webapp/WEB-INF/views/user1/modify.jsp
+++ spring/ch04/src/main/webapp/WEB-INF/views/user1/modify.jsp
@@ -2,18 +2,15 @@
   Created by IntelliJ IDEA.
   User: kimminimanimu
   Date: 2025. 7. 20.
-  Time: 오후 11:22
+  Time: 오후 11:26
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<head>
-    <title>greeting</title>
-</head>
-<body>
-    <h3>Greeting</h3>
-    <a href="/ch04/hello">hello</a>
-    <a href="/ch04/welcome">welcome</a>
-    <a href="/ch04/greeting">greeting</a>
-</body>
+  <head>
+    <title>Title</title>
+  </head>
+  <body>
+  
+  </body>
 </html>
