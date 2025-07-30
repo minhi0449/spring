dc3edcc746468db4aa40f8c301ffb2f9d23e9bee
minhi0449 <minhi0449@gmail.com>
2025-07-30 14:25:36 +0900

    Mapper

--- spring/ch05/src/main/java/com/ch05/dao/User1Mapper.java
+++ spring/ch05/src/main/java/com/ch05/dao/User1Mapper.java
@@ -1,4 +1,15 @@
 package com.ch05.dao;
 
-public class User1Mapper {
+import com.ch05.dto.User1DTO;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+// MyBatis Scanning 을 위해 @Mapper 선언
+@Mapper
+public interface User1Mapper {
+    public void insertUser1(User1DTO user1DTO);
+    public List<User1DTO> selectUser1s();
+    public User1DTO selectUser1(String uid);
+    public void updateUser1(User1DTO user1DTO);
+    public void deleteUser1(String uid);
 }
